a75464d1c2cf93690a93a7bd59fdcad4d2deeb1c
huy.nq <huyquy2812@gmail.com>
2022-10-16 22:46:31 +0700

    add Header

--- backend/src/models/Reviews.js
+++ backend/src/models/Reviews.js
@@ -1,10 +1,15 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const ReviewSchema = new Schema({
-    reservationId: { type: Schema.Types.ObjectId, ref: "Reservation" },
-    rating: { type: Number },
-    comment: { type: String },
-});
+const ReviewSchema = new Schema(
+    {
+        reservationId: { type: Schema.Types.ObjectId, ref: "Reservation" },
+        rating: { type: Number },
+        comment: { type: String },
+    },
+    {
+        timestamps: true,
+    }
+);
 
 module.exports = mongoose.model("Reviews", ReviewSchema);
--- frontend/src/layouts/DefaultLayout.tsx
+++ frontend/src/layouts/DefaultLayout.tsx
@@ -1,1 +1,7 @@
-export {}
+import { Header } from "../components";
+
+function DefaultLayout({ children }: { children: React.ReactNode | React.ReactNode[] }) {
+    return <Header />;
+}
+
+export default DefaultLayout;
