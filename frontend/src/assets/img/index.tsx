6d5805c8cec23cc08c75b70cbbd4d3a1b69bd309
toan.dg <guitartgiatoan@gmail.com>
2022-10-18 09:43:03 +0700

    Add Footer.

--- frontend/src/assets/img/index.tsx
+++ frontend/src/assets/img/index.tsx
@@ -1,9 +1,11 @@
 import airbnbLogo from "./airbnb-logo.svg";
 import avatar from "./avatar.svg";
+import global from "./global.svg";
 
 const Image = {
     logo: airbnbLogo,
     avatar: avatar,
+    global: global,
 };
 
 export default Image;
--- frontend/src/components/Footer/Footer.tsx
+++ frontend/src/components/Footer/Footer.tsx
@@ -1,9 +1,22 @@
-function Footer() {
-    return (
-        <footer>
-            <h2>Footcccccccccer</h2>
-        </footer>
-    )
+import Images from "../../assets/img";
+
+const Footer: React.FC = () => {
+  return (
+    <div className="flex">
+      <div className="flex">
+        <p>© 2022 Airbnb, Inc.</p>
+        <a href="" className="ml-3">Privacy</a>
+        <a href="" className="ml-3">Terms</a>
+        <a href="" className="ml-3">Sitemap</a>
+        <a href="" className="ml-3">Destinations</a>
+      </div>
+      <div className="flex ml-auto">
+        <a href="" className="flex inline-block mr-3"><img src={Images.global} alt="error" />English (US)</a>
+        <a href="" className="flex inline-block mr-3"><img src="" alt="error" />USD</a>
+        <a href="" className="flex inline-block">Support & resources<img src="" alt="error" /></a>
+      </div>
+    </div>
+  )
 }
 
-export default Footer;
+export default Footer
