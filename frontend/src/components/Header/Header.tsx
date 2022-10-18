f1f8ebc73096f1737b5a13576ddb85781b050ee3
huy.nq <huyquy2812@gmail.com>
2022-10-18 15:26:03 +0700

    add active menu

--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -17,7 +17,7 @@ function Header() {
     };
 
     // const renderItems = (attrs: any) => (
-        
+
     // );
 
     return (
@@ -32,29 +32,34 @@ function Header() {
                 <button className="w-[48px] p-[12px] rounded-[50%] hover:bg-[rgb(247,247,247)]">
                     <FontAwesomeIcon icon={faGlobe} />
                 </button>
-                <Tippy interactive hideOnClick trigger="click" placement="top-end" render={(attrs) => (
-                    <div className="w-[240px] bg-white rounded-[12px] shadow-[0_2px_16px_rgba(0,0,0,0.12)]" tabIndex={-1} {...attrs} >
-                    <div className="py-2">
-                        {authItems.map((item, index) => {
-                            return (
-                                <Link to="" key={index} className="block p-[12px] bg-transparent hover:bg-[#F7F7F7]">
-                                    {item}
-                                </Link>
-                            );
-                        })}
-                    </div>
-                    <hr className="h-[2px] bg-[#DDDDDD]" />
-                    <div className="py-2">
-                        {hostItems.map((item, index) => {
-                            return (
-                                <Link to="" key={index} className="block p-[12px] bg-transparent hover:bg-[#F7F7F7]">
-                                    {item}
-                                </Link>
-                            );
-                        })}
-                    </div>
-                </div>
-                )}>
+                <Tippy
+                    interactive
+                    trigger="click"
+                    placement="bottom-end"
+                    render={(attrs) => (
+                        <div className="w-[240px] bg-white rounded-[12px] shadow-[0_2px_16px_rgba(0,0,0,0.12)]" tabIndex={-1} {...attrs}>
+                            <div className="py-2">
+                                {authItems.map((item, index) => {
+                                    return (
+                                        <button key={index} className="block w-full text-left p-[12px] bg-transparent hover:bg-[#F7F7F7]">
+                                            {index === 0 ? <span className="font-bold">{item}</span> : <span>{item}</span>}
+                                        </button>
+                                    );
+                                })}
+                            </div>
+                            <hr className="h-[2px] bg-[#DDDDDD]" />
+                            <div className="py-2">
+                                {hostItems.map((item, index) => {
+                                    return (
+                                        <Link to="" key={index} className="block p-[12px] bg-transparent hover:bg-[#F7F7F7]">
+                                            {item}
+                                        </Link>
+                                    );
+                                })}
+                            </div>
+                        </div>
+                    )}
+                >
                     <button onClick={handleShowTippy} className="flex items-center px-[12px] py-[5px] rounded-[999px] border-solid border-[1px] border-[#DDDDDD] hover:shadow-[0_2px_4px_rgba(0,0,0,0.18)]">
                         <FontAwesomeIcon icon={faBars} className="mr-2" />
                         <img src={Images.avatar} alt="avatar" className="w-[30px] h-[30px]" />
