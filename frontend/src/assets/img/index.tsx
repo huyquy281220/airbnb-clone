8af0ed976334670a5e4a2fd587c9b4e8fe2130a6
toan.dg <guitartgiatoan@gmail.com>
2022-10-18 15:42:47 +0700

    Add Modal Popup.

--- frontend/src/assets/img/index.tsx
+++ frontend/src/assets/img/index.tsx
@@ -1,11 +1,13 @@
 import airbnbLogo from "./airbnb-logo.svg";
 import avatar from "./avatar.svg";
 import global from "./global.svg";
+import iconPopup from "./icon-popup.svg";
 
 const Image = {
     logo: airbnbLogo,
     avatar: avatar,
     global: global,
+    iconPopup: iconPopup,
 };
 
 export default Image;
